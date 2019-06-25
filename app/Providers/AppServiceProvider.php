ec871f9fca11978d7a6b2165bec6d1f45e17addd
panyushovpavel <panyushov@gmail.com>
2019-06-25 23:27:00 +0200

    Registers app services. Adds queue event listener in the app bootstrapper.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+use App\Services\DatabaseQueueService;
+use App\Services\MusementService;
+use App\Services\XMLWriterService;
+use Illuminate\Support\Facades\Queue;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,16 +17,30 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->singleton('App\Services\DatabaseQueueService', function () {
+            return new DatabaseQueueService();
+        });
+
+        $this->app->singleton('App\Services\MusementService', function ($app) {
+            return new MusementService($app->make('GuzzleHttp\Client'));
+        });
+
+        $this->app->singleton('App\Services\XMLWriterService', function ($app) {
+            return new XMLWriterService();
+        });
     }
 
     /**
      * Bootstrap any application services.
      *
+     * @param DatabaseQueueService $qServ
      * @return void
      */
-    public function boot()
+    public function boot(DatabaseQueueService $qServ)
     {
-        //
+        // Queue listener
+        Queue::after(function () use ($qServ) {
+            $qServ->queueIsComplete();
+        });
     }
 }
