74f6f7257e7b6a68dbb3f6e2369f60028a97513d
panyushovpavel <panyushov@gmail.com>
2019-06-25 23:45:47 +0200

    Adds 'email separated by comma' validation

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,7 @@ use App\Services\DatabaseQueueService;
 use App\Services\MusementService;
 use App\Services\XMLWriterService;
 use Illuminate\Support\Facades\Queue;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -38,6 +39,26 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(DatabaseQueueService $qServ)
     {
+        // Extending validation
+        Validator::extend("emails", function ($attribute, $value, $parameters) {
+            $rules = [
+                'email' => 'sometimes|email',
+            ];
+            $emails = explode(",", $value);
+
+            foreach ($emails as $email) {
+                $email = trim($email);
+                $data = [
+                    'email' => $email
+                ];
+                $validator = Validator::make($data, $rules);
+                if ($validator->fails()) {
+                    return false;
+                }
+            }
+            return true;
+        }, "Every comma separated recipient should be a valid email address");
+
         // Queue listener
         Queue::after(function () use ($qServ) {
             $qServ->queueIsComplete();
