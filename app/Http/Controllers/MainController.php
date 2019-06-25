8b98c082d062f129a36ee5d4a38780703ddd6075
panyushovpavel <panyushov@gmail.com>
2019-06-25 23:50:43 +0200

    Adds main app controller

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\SMTPConfig;
+use App\Services\DatabaseQueueService;
+use Illuminate\Http\Request;
+
+class MainController extends Controller
+{
+    /**
+     * Processes SMTP config alteration request.
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function processConfig(Request $request)
+    {
+        $request->validate([
+            "host" => "required|max:255",
+            "port" => "required|numeric|between:0,65535",
+            "username" => "required|max:255",
+            "password" => "required|max:255",
+            "encryption" => "sometimes|max:255",
+            "from" => "required|email",
+        ]);
+
+        $input = $request->input();
+        SMTPConfig::cleanCorruption();
+        SMTPConfig::saveConfig($input);
+
+        return redirect()->route('muse.config');
+    }
+
+    /**
+     * Receives and validates request for sitemap generation.
+     *
+     * @param Request $request
+     * @param DatabaseQueueService $qServ
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function startGeneration(Request $request, DatabaseQueueService $qServ)
+    {
+        $request->validate([
+            "emails" => 'sometimes|emails',
+        ]);
+
+        SMTPConfig::setRecipients($request->input("emails"));
+        SMTPConfig::cleanCorruption();
+
+
+        $qServ->dispatchCitiesJobs();
+
+        return redirect()->route('muse.generator');
+    }
+
+    /**
+     * Renders Sitemap Generator page.
+     *
+     * @param DatabaseQueueService $qServ
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function generator(DatabaseQueueService $qServ)
+    {
+        $warnings = [];
+        $config = SMTPConfig::config();
+        $this->checkConfWarnings($config, $warnings);
+
+        $vars = [
+            "warnings" => $warnings,
+            "flgGenerating" => $qServ->generationInProgress()
+        ];
+        return view('generator', $vars);
+    }
+
+    /**
+     * Service method. Checks smtp for error messages, puts them in the warnings
+     * array and returns them in the main controller methods.
+     *
+     * @param $config
+     * @param $warnings
+     * @return void
+     */
+    private function checkConfWarnings($config, &$warnings)
+    {
+        if (SMTPConfig::exists() and $config->corrupt) {
+            $warnings[] = sprintf("During the previous generation there were some problems with email notifications. 
+            Please fix configuration if necessary. Exception message: %s", $config->corrupt);
+        }
+    }
+
+    /**
+     * Composes and renders SMTP config page.
+     *
+     * @param DatabaseQueueService $qServ
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function config(DatabaseQueueService $qServ)
+    {
+        $warnings = [];
+        $config = SMTPConfig::config();
+        $this->checkConfWarnings($config, $warnings);
+
+        $vars = [
+            "warnings" => $warnings,
+            "config" => $config,
+            "flgGenerating" => $qServ->generationInProgress(),
+        ];
+
+        return view('config', $vars);
+    }
+
+    /**
+     * Root route. Based on the config existence condition
+     * redirects users either directly on the sitemap generation
+     * page or on the SMTP configuration page.
+     *
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function index()
+    {
+        if (!SMTPConfig::exists()) {
+            return redirect()->route('muse.config');
+        } else {
+            return redirect()->route('muse.generator');
+        }
+    }
+}
