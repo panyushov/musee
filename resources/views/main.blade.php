a2ad54e99059baac187b521c380dad0f0ee8e4da
panyushovpavel <panyushov@gmail.com>
2019-06-26 08:02:01 +0200

    Corrects main template page title

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>µSe - SiteMaps</title>
+    <title>µSée - SiteMaps</title>
     <link rel="stylesheet"
           href="/css/app.css">
 </head>
